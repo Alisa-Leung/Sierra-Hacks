59e4ae53c796ee6078e3e3a5fa71bc527df2afd1
Alisa Leung <alisal.singyee@gmail.com>
2025-11-15 10:38:47 -0800

    updated logo

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const modeDropdown = document.getElementById("modeDropdown");
-modeDiv = document.getElementById("modeDiv");
-
-modeDropdown.addEventListener("change", (event) => {
-    const divContent = document.createElement("p");
-    divContent.innerHTML = "Visually Impaired";
-    if (modeDropdown.value == "visuallyImpaired") {
+document.addEventListener("DOMContentLoaded", () => {
+    //this code adds content to the html file based on content
+    const modeDropdown = document.getElementById("modeDropdown");
+    const modeDiv = document.getElementById("modeDiv");
+    modeDropdown.addEventListener("change", (event) => {
+        const existingContent = document.getElementById("divContent");
+        if (existingContent){
+            existingContent.remove();
+        }
+        const divContent = document.createElement("p");
+        if (event.target.value){
+            divContent.id = "divContent";
+            if (event.target.value === "visuallyImpaired"){
+                divContent.innerHTML = "Visually Impaired";
+            } else if (event.target.value === "hardOfHearing"){
+                divContent.innerHTML = "Hard of Hearing";
+            } else if (event.target.value === "speakingDifficulty"){
+                divContent.innerHTML = "Speaking Difficulty";
+            }
+        }
         modeDiv.appendChild(divContent);
-    }
+    })
 })
